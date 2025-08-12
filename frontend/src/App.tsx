e2e4f0f7a4589b1578f823fb2f58bde8b86164ea
Krutagna Patel <krutagnapatel31@gmail.com>
2025-08-12 16:18:23 +0530

    add query client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
 import '@mantine/core/styles.css';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { MantineProvider } from '@mantine/core';
 import { Router } from './Router';
 import { theme } from './theme';
 
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
     <MantineProvider theme={theme}>
-      <Router />
+      <QueryClientProvider client={queryClient}>
+        <Router />
+      </QueryClientProvider>
     </MantineProvider>
   );
 }
