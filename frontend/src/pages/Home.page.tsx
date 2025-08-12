5de7f63e318c0cf2a851f4d5a3122857e851ecf4
Krutagna Patel <krutagnapatel31@gmail.com>
2025-08-12 16:17:07 +0530

    feat(backend): setup backend with employee data

--- frontend/src/pages/Home.page.tsx
+++ frontend/src/pages/Home.page.tsx
@@ -1,11 +1,32 @@
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
-import { Welcome } from '../components/Welcome/Welcome';
+import { useQuery } from '@tanstack/react-query';
+import { Title } from '@mantine/core';
+import EmployeesList from '@/components/EmployeesList/EmployeesList';
+import { Employee } from '@/types/types';
 
 export function HomePage() {
+  const { data, isPending, error } = useQuery<Employee[]>({
+    queryKey: ['employees'],
+    queryFn: () => fetch('http://localhost:3000/api/employees').then((res) => res.json()),
+  });
+
+  if (isPending) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (error) {
+    <h2>An error has occured: ${error.message}</h2>;
+  }
+
+  if (!data) {
+    return <h2>No employees found</h2>;
+  }
+
   return (
     <>
-      <Welcome />
-      <ColorSchemeToggle />
+      <Title ta="center" mb="lg">
+        Employees Directory
+      </Title>
+      <EmployeesList employees={data} />
     </>
   );
 }
